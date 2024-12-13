7b55fd6e4f72d6655db4f1dd0fbe02a1152855e3
João Marcos <joaoteixeirasilva33@gmail.com>
2024-12-12 22:54:44 -0300

    refactor: send html archive

--- aula20/hello-world.ts
+++ aula20/hello-world.ts
@@ -1,6 +1,6 @@
-import express from 'express'
+import express, { Request } from 'express'
 import {config} from 'dotenv'
-import {join} from 'path'
+import path from 'path'
 import {readFileSync} from 'fs'
 
 config()
@@ -8,11 +8,14 @@ const app = express()
 const url = process.env.API_BASE_URL ?? 'http://localhost'
 const port = process.env.API_PORT ?? 3000
 
-app.get('/', (req, res) => {    
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (request,  response) => {    
     // res.send('Hello World!!')
     // res.send('<h1>Hello World!!</h1>')
-    const homepath = join(__dirname, 'home.html')
-    const read
+    const homepath = path.join(__dirname, 'home.html')
+    const readHome = readFileSync(homepath)
+    return response.status(200).send(readHome)
 })
 
 app.listen(port, () => {
